c86e19bfdf71cb73731a88f7ee1184f5c99249d1
Abhishek-76 <avinashgautam8923933406@gmail.com>
2022-06-08 12:13:46 +0530

    home

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -3,25 +3,38 @@ import './Home.css'
 const Home=()=>
 {
     return(
-      <div>
-        <div className='container'>
-          <div className='description'>
-            <div >
-            <h1>Plants Will make Your life Better</h1>
-            <h4> Create incredible plant design for .....</h4>
-          </div>
-          <div style={{float:'right'}}>
-            <img src={homeimg} height='400' alt=""/>
-          </div>
-        </div>
-        <div>
-          FollowUS ━━ Fb Insta Twitter
-        </div>
-        <div>
-          <button>Explore ↘</button>
-        </div>
-      
-        </div>
+      <div className='home'>
+        <div className="home__container container grid">
+          <img src={homeimg} alt="" className="home__img"/>
+
+            <div className="home__data">
+              <h1 className="home__title">
+                Plants will make  your life better
+              </h1>
+              <p className="home__description">
+                Create incredible plant design for your offices or apastaments. 
+                Add fresness to your new ideas.
+               </p>
+              <a href="#about" className="button button--flex">
+                Explore ↘
+              </a>
+              </div>
+
+              <div className="home__social">
+                <span className="home__social-follow">Follow Us</span>
+                  <div className="home__social-links">
+                    <a href="https://www.facebook.com/" target="_blank" className="home__social-link">
+                      Fb
+                    </a>
+                    <a href="https://www.instagram.com/" target="_blank" className="home__social-link">
+                      Insta
+                    </a>
+                    <a href="https://twitter.com/" target="_blank" className="home__social-link">
+                      twitter
+                    </a>
+                  </div>
+                </div>
+              </div>
       </div>
     )
 }
