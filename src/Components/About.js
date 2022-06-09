1e097e8676e39053a086915f3d8e78b893a0375d
Abhishek-76 <avinashgautam8923933406@gmail.com>
2022-06-09 10:50:56 +0530

    about

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,70 +1,46 @@
 import aboutimg from '../Assets/about.png'
-import classes from './About.css'
+import  './About.css'
 const About=()=>{
     return(
       
-      <div className='conatiner'>
-      <div className='description'>
-        <div>
-          <img src={aboutimg} height='400' alt='about'/>
-        </div>
-      
-        <div>
-        <h1><p>Who we really are & why choose us</p></h1>
-        <h3><p> We have over 4000+ unbiased reviews and 
-          customers trust our plant process and delivery service every time.</p></h3>
-        
-        <ul className={classes.ul}>
-          <li>We always deliver on time.;</li>
-          <li>We give you guides to protect and care for your plants.</li>
-          <li>We always come over for a check-up after sale.</li>
-          <li>100% money back guarantees.</li>
-               
-        </ul>
-        <p>&ensp;</p>
-        <button>Shop Now ↘</button>
-        </div>
-        </div>
-        <div className='card'>
-        <div className="parent">
-          <div className="card_title">
-            <h2>Steps to start your plans off right</h2>
-          </div>
-          
-          <div className="div1"> 
-            <div className='number_box'>
-              01
-            </div>
-            <div className='Card_description'>
-              <h3><p>Choose Plant </p></h3>
-              <p>  We have severla variants plants you can choose from</p>
-            </div>
-          
-          </div>
-          <div className="div2"> 
-            <div className='number_box'>
-             02
-            </div>
-            <div  className='Card_description'>
-              <h3><p>Place Order&ensp;</p></h3>
-              <p>Once your Order is set ,we move to the next step which is the shipping</p>
-            </div>
-          </div>
-          <div className="div3"> 
-            <div className='number_box'>
-              03
-            </div >
-              <div className='Card_description'>
-              <h3><p>Get Plants delivered </p></h3>
-              <p>Our delivery process is easy , you recieve the plant direct to your door</p>
-              </div>
-              
-           </div>
-        </div>
-      
-      </div>
-  
-      </div>
+      <section class="about section container" id="about">
+        <div class="about__container grid">
+          <img src={aboutimg} alt="" class="about__img"/>
+          <div class="about__data">
+            <h2 class="section__title about__title">
+              Who we really are & <br/> why choose us
+            </h2>
+
+            <p class="about__description">
+              We have over 4000+ unbiased reviews and our customers 
+              trust our plant process and delivery service every time
+            </p>
+
+            <div class="about__details">
+              <p class="about__details-description">
+              <i class="ri-checkbox-fill about__details-icon"></i>
+                                We always deliver on time.
+                            </p>
+                            <p class="about__details-description">
+                                <i class="ri-checkbox-fill about__details-icon"></i>
+                                We give you guides to protect and care for your plants.
+                            </p>
+                            <p class="about__details-description">
+                                <i class="ri-checkbox-fill about__details-icon"></i>
+                                We always come over for a check-up after sale.
+                            </p>
+                            <p class="about__details-description">
+                                <i class="ri-checkbox-fill about__details-icon"></i>
+                                100% money back guaranteed.
+                            </p>
+                        </div>
+
+                        <a href="#" class="button--link button--flex">
+                            Shop Now <i class="ri-arrow-right-down-line button__icon"></i>
+                        </a>
+                    </div>
+                </div>
+            </section>
     
     )
 }
