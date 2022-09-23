0bcffbc3496a045b2579f58c715e57b3ffb37514
Abhishek-76 <avinashgautam8923933406@gmail.com>
2022-09-23 11:33:47 +0530

    frontEnd

--- src/App.js
+++ src/App.js
@@ -1,32 +1,35 @@
 import React from 'react';
-import About from './Components/About/About';
-import Header from './Components/Header';
+//import About from './Components/About/About';
+import Header from './Components/Header/Header';
 import './App.css'
 import Home from './Components/Home';
-import Product from './Components/Product';
-import Questions from './Components/Questions';
+//import Product from './Components/Product';
+//import Questions from './Components/Questions';
 function App() {
   return (
-    <div className='container'>
-      <div>
+    <>
+      <div className='div-container'>
         <Header/>
       </div>
-      <div>
+      <div className='div-container'>
         <Home/>
       </div>
-      <div>
-        <About/>
-      </div>
-      <div>
-        <Product/>
-      </div>
-      <div>
-        <Questions/>
-      </div>
       
-    </div>
+    </>
         
   );
 }
 
 export default App;
+
+//      <div className='div-container'>
+//        <About/>
+//      </div>
+  //    <div className='div-container'>
+  //      <Product/>
+      //</div>
+    //  <div className='div-container'>
+        //<Questions/>
+      //</div>
+      //<i class="fa-brands fa-facebook-f"></i>
+      //<i class="fa-solid fa-moon"></i>
---
+++ src/Components/Header/Header.js
@@ -0,0 +1,43 @@
+import { MdDarkMode } from "react-icons/md";
+import { TbLeaf } from "react-icons/tb";
+import './Header.css'
+const Header=()=>
+{
+    
+    return (
+      <header className='header'>
+        
+        <nav className='nav nav_container'>   
+          <a href="#logo" className="nav__logo">
+            <TbLeaf className=" nav__logo-icon"/>Plantx
+          </a>  
+          
+          <div className="nav__menu" >
+            <ul className="nav__list">
+              <li className="nav__item">
+                <a href="#home" className="nav__link active-link">Home</a>
+              </li>
+              <li className="nav__item">
+                <a href="#about" className="nav__link">About</a>
+              </li>
+              <li className="nav__item">
+                <a href="#products" className="nav__link">Products</a>
+              </li>
+              <li className="nav__item">
+                <a href="#faqs" className="nav__link">FAQs</a>
+              </li>
+              <li className="nav__item">
+                <a href="#contact" className="nav__link">Contact Us</a>
+              </li>
+            </ul>
+          </div>
+         
+          <div className="nav__btns">
+            <MdDarkMode/>          
+          </div>    
+        </nav>
+      </header>
+    )
+
+}
+export default Header;
