a4d8d4ef1c7fadcd4bd229709d31b39ba4f4b65b
Abhishek-76 <avinashgautam8923933406@gmail.com>
2022-10-10 23:01:28 +0530

    responsiveHeader

--- src/App.js
+++ src/App.js
@@ -1,19 +1,15 @@
 import React from 'react';
 import About from './Components/About/About';
 import Header from './Components/Header/Header';
-import './App.css';
 import Home from './Components/Home/Home';
 import Product from './Components/Product/Product';
 import Contact from './Components/Contact/Contact';
 import Footer from './Components/Footer/Footer';
-
 import AccordionTemp from './Components/Question/AccordionTemp';
 import StepsCard from './Components/About/StepsCard';
 import ScrollToTop from './Components/ScrollToTop/ScrollToTop';
 
 function App() {
-  
-  
   
   return (
     
